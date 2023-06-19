a430029f3d210d1e95bd1a64c67b570573558520
hoangtien2093 <133217369+hoangtien2093@users.noreply.github.com>
2023-06-19 18:54:34 +0700

    Add files via upload

---
+++ jsDe2.js
@@ -0,0 +1,56 @@
+function kiemtra(){
+    // var sFirstname = document.getElementById("txtFname").value;
+    // var reFirstname = /^[A-Za-z0-9]+$/;
+
+    //     if(!reFirstname.test(sFirstname)){
+    //     alert("First name cannot left blank! ");
+    //     document.getElementById("txtFname").focus();
+    //     return false;
+    //     }
+
+    // var sLastname = document.getElementById("Lastname").value;
+    // var reLastname = /^[A-Za-z]+$/;
+
+    //     if(!reLastname.test(sLastname)){
+    //     alert("Last name cannot left blank! ");
+    //     document.getElementById("Lastname").focus();
+    //     return false;
+    //     }
+
+    // var sEmail = document.getElementById("Email").value;
+    // var reEmail = /^[A-Za-z]\w+[@]\w+[.]\w{3}$/; 
+    //     if(!reEmail.test(sEmail)){
+    //         alert("Email is not invalid! ");
+    //         document.getElementById("Email").focus();
+    //         return false;
+    //    }
+
+    // var sPhone = document.getElementById("Phone").value;
+    // var rePhone = /^\d{8,12}$/;
+  
+    //     if(!rePhone.test(sPhone)){
+    //         alert("Phone must be in range [8 - 12]!");
+    //         document.getElementById("Phone").focus();
+    //         return false;
+    //     }
+
+    // var sPassword = document.getElementById("Password").value;
+    // var rePassword = /^\w{3,12}$/;
+    //         //pass
+    //     if(!rePassword.test(sPassword)){
+    //         alert("Password is invalid! ");
+    //         document.getElementById("Password").focus();
+    //         return false;
+    //     }
+
+    //     var sCFPassword = document.getElementById("CFPassword").value;
+    //     var reCFPassword = /^\w{3,12}$/;
+    //             //pass
+    //         if(sPassword != sCFPassword ){
+    //             alert("Confirmed Password is invalid! ");
+    //             document.getElementById("Password").focus();
+    //             return false;
+    //         }
+
+
+}
